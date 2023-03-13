1ca80261b7fd025b2adf02abab199c3d561831b1
youssefshadow <youssef20ben@gmail.com>
2023-03-13 11:23:45 +0100

    exo échauffement

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+
+$nbr1 = 10;
+$nbr2 = 23;
+$total = $nbr1 + $nbr2;
+
+
+echo "La somme de $nbr1 + $nbr2 est égale à $total.<br>";
+
+
+$nbr1 = 5;
+$nbr2 = 8;
+
+
+$total = $nbr2 - $nbr1;
+
+
+echo "Le reste de la soustraction $nbr2 et $nbr1 est égale à $total.";
+?>
