9d2e4095d846f14066d8e785bb41913fbefbb4bd
youssefshadow <youssef20ben@gmail.com>
2023-03-13 14:01:27 +0100

    exo cast

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 $nbr1 = 10;
 $nbr2 = 23;
 $total = $nbr1 + $nbr2;
-echo "La somme de $nbr1 + $nbr2 est égale à $total.<br>";
+echo "<h3>La somme de $nbr1 + $nbr2 est égale à $total.<br></h3>";
 $nbr1 = 5;
 $nbr2 = 8;
 $total = $nbr2 - $nbr1;
@@ -21,6 +21,43 @@ echo "Le reste de la soustraction $nbr2 et $nbr1 est égale à $total.<br>";
 //     $nbr2 = 26;
 //     $total = $nbr2-$nbr1;
 //     echo $total;
+//     Exemple de concaténation :
+ $nbr1 = 10;
+    $nbr2 = 22;
+    $resultat = "Le résultat est égal à : ";
+    //version encadrée en double 
+    echo "$nbr1 + $nbr2 est égal à $resultat<br>";
+    //version encadrée en simple
+    echo ''.$nbr1.' +  '.$nbr2.' est égal à '.$resultat.'<br>';
+    //concaténation avec opérateur .
+    echo $nbr1." + ".$nbr2." est égal à : ".$resultat."<br>"; 
+    //opération de concaténation
+    $nbr1 .= "texte";
 
+   
+    // $mot = "adrar";
+
+    // $majuscule = implode("", array_map(function ($i, $char) {
+    // return $i % 2 == 0 ? strtolower($char) : strtoupper($char);
+    // }, array_keys(str_split($mot)), str_split($mot)));
+
+    // echo $majuscule;
+    $mot = "adrar";
+    $majuscule = implode("", array_map(function ($char, $i) {
+    return $i % 2 == 0 ? strtolower($char) : strtoupper($char);
+    }, str_split($mot), array_keys(str_split($mot))));
+
+    echo $majuscule;
+    function maj($word) {
+        return strtoupper($word[0]) . substr($word, 1);
+    }
+    
+    $result = maj("hello");
+    echo $result; 
+    //strlen c'est l'équibalent de length en javascript
+    for ($i=0; $i < strlen($result) ; $i++) { 
+       echo $i;
+    };
+    
 ?>
 
