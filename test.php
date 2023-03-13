57f572f28ff68e32e344e98d793895bb25d1d991
youssefshadow <youssef20ben@gmail.com>
2023-03-13 11:55:07 +0100

    exo cast

--- test.php
+++ test.php
@@ -1,19 +1,26 @@
 <?php
-
 $nbr1 = 10;
 $nbr2 = 23;
 $total = $nbr1 + $nbr2;
-
-
 echo "La somme de $nbr1 + $nbr2 est égale à $total.<br>";
-
-
 $nbr1 = 5;
 $nbr2 = 8;
-
-
 $total = $nbr2 - $nbr1;
+echo "Le reste de la soustraction $nbr2 et $nbr1 est égale à $total.<br>";
+    $nbr1 = "10";
+    $nbr1 = (int)$nbr1; 
+    echo gettype($nbr1);
+// Correction Exercice 1 variables : 
+// <?php
+//     $nbr1 = 10;
+//     $nbr2 = 23;
+//     $total;
+//     $total = $nbr1+$nbr2;
+//     echo $total.'<br>';
+//     $nbr1 = 42;
+//     $nbr2 = 26;
+//     $total = $nbr2-$nbr1;
+//     echo $total;
 
-
-echo "Le reste de la soustraction $nbr2 et $nbr1 est égale à $total.";
 ?>
+
