55c10c7a724e6ffc6867b24248a69824f5fb62bd
youssefshadow <youssef20ben@gmail.com>
2023-03-15 11:01:29 +0100

    form

--- form1.php
+++ form1.php
@@ -1,5 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
 <body>
-	<h1>Formulaire de malade</h1>
+<style>
+<?php include 'style.css'; ?>
+</style>
+<h1>Formulaire de malade</h1>
 	<form method="post" action="recup.php">
 		<label for="nbr1">Nombre 1 :</label>
 		<input type="text" name="nbr1" id="nbr1"><br><br>
@@ -8,3 +20,7 @@
 		<input type="submit" name="submit" value="Envoyer">
 	</form>
 </body>
+</html>
+
+
+
