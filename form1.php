759778c6fe4ed140f3675dd8f8bfb1c346166fb8
youssefshadow <youssef20ben@gmail.com>
2023-03-15 10:47:20 +0100

    form

---
+++ form1.php
@@ -0,0 +1,10 @@
+<body>
+	<h1>Formulaire de malade</h1>
+	<form method="post" action="recup.php">
+		<label for="nbr1">Nombre 1 :</label>
+		<input type="text" name="nbr1" id="nbr1"><br><br>
+		<label for="nbr2">Nombre 2 :</label>
+		<input type="text" name="nbr2" id="nbr2"><br><br>
+		<input type="submit" name="submit" value="Envoyer">
+	</form>
+</body>
